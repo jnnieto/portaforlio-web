9d51a375ad277d2e8c13f73df2c6ac16187726a9
Nicolás <niconieto1506@gmail.com>
2021-03-28 00:22:07 -0500

    Skils

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -20,9 +20,9 @@ export class HomeComponent implements OnInit {
     this.aboutMe = "Software development student passionate about the world of technology, " +
     "eager to continue learning new development tools that strengthen my professional skills.";
     this.frontend = "I'm a front developer intereed in knowing the new stantards of UI design," +
-    "with basic knowledge in HTML, CSS and JS."
+    " with basic knowledge in HTML, CSS and JS."
     this.logic = "I consider myself a programmer capable of solving any type of problem that requires logic and algorithmic skills. "
-    this.backend = ""
+    this.backend = "I consider myself a back developer capable of adapting to any programming language. Based on logical abtraction, I seek to implement solutions for the good management of information. "
   }
 
   ngOnInit(): void {
